9a1572c3241575fc1b6caed79f3de1ca7cb77996
aponteloaizam <aponteloaizam@gmail.com>
2025-07-01 13:38:23 +0200

    Añadiendo ejercicio 2

--- index.php
+++ index.php
@@ -10,4 +10,23 @@ for ($i = 0; $i < count($array); $i++) {
     echo $array[$i] . " ";
 }
 
+echo "\n\nEjercicio 2\n";
+
+$x = array(10, 20, 30, 40, 50, 60);
+
+echo "Imprimiendo el array original.\n";
+
+print_r($x);
+
+echo "El tamaño del array es de " . count($x) . " valores.\n";
+
+unset($x[4]);
+$x = array_values($x);
+
+echo "Imprimiendo el array tras eliminar elemento y reindexar claves.\n";
+
+print_r($x);
+
+echo "El tamaño del nuevo array es de " . count($x) . " valores.\n";
+
 ?>
